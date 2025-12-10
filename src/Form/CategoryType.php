9ba7447a92835275c7d768acd66991aa406aa615
nino-cl <claudepierre.nino@esh64.fr>
2025-12-10 15:40:42 +0100

    crud category commencé

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,32 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
 use App\Entity\Category;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ArticleType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('category',EntityType::class,[
-                'class' => Category::class,
-                'choice_label' => 'title',
-                'label' => 'Catégorie'
-            ])
+            ->add('title')
+            ->add('description')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Article::class,
+            'data_class' => Category::class,
         ]);
     }
 }
