fa9076d86db7f9dc6c1241cd85fb7346f613ef37
nino-cl <claudepierre.nino@esh64.fr>
2025-12-10 15:06:04 +0100

    category_id ajouté à la bdd

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -20,6 +20,9 @@ class Article
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private ?string $price = null;
 
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?Category $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -45,6 +48,18 @@ class Article
     public function setPrice(string $price): static
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
 
         return $this;
     }
