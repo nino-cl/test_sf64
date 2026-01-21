5c8d5bfb88d9d7bf66574041b040e8e5a54dc268
nino-cl <claudepierre.nino@esh64.fr>
2026-01-21 17:12:13 +0100

    login et register fini, début vérif (ma version)

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -15,9 +16,19 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(
+        min : 5,
+        max : 50,
+        minMessage : "Le nom d'un article doit comporter au moins {{ limit }} caractères",
+        maxMessage : "Le nom d'un article doit comporter au plus {{ limit }} caractères"
+    )]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Assert\NotEqualTo(
+        value : 0,
+        message : "Le prix d’un article ne doit pas être égal à 0 "
+    )]
     private ?string $price = null;
 
     #[ORM\ManyToOne(inversedBy: 'articles')]
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,16 +6,30 @@ use App\Entity\Article;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'required' => true,
+                    'label' => 'Nom de l\'article',
+                    'placeholder' => 'Entrez le nom de l\'article : entre 5 et 50 caractères',
+                ],
+            ])
+            ->add('price', TextType::class, [
+                'attr' => [
+                    'required' => true,
+                    'label' => 'Prix de l\'article',
+                    'placeholder' => 'Entrez le prix de l\'article : il doit être supérieur à 0€',
+                ],
+            ])
             ->add('category',EntityType::class,[
                 'class' => Category::class,
                 'choice_label' => 'title',
